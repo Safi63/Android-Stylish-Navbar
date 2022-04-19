e6b0bee27bdfccd70886dbe8fa924b24591ba20a
Safi Ullah Muhammad <safifiverr786@gmail.com>
2022-04-18 23:52:30 -0700

    Login page with simple validation is added

--- app/src/main/java/com/example/bottom_navbar/HomeFragment.kt
+++ app/src/main/java/com/example/bottom_navbar/HomeFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 
 class HomeFragment : Fragment() {
+
+    private var root : View? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        var email = rootView.findViewById<EditText>(R.id.editTextEmail).text
+        var password = rootView.findViewById<EditText>(R.id.editTextPassword).text
+
+
+            rootView.findViewById<Button>(R.id.loginButton).setOnClickListener {
+
+            if (email.isNullOrBlank()&&password.isNullOrBlank()){
+                Toast.makeText(context,"Please fill required fields",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(context,"${email} is logged in!!",Toast.LENGTH_LONG).show()
+            }
+        }
+        return rootView
     }
 
 
